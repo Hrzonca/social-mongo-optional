5914f19432ca8776564fd29adb9c6f7043340659
Haleigh Rzonca <hgielah@gmail.com>
2022-12-20 13:48:41 -0800

    set up files and started linking

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+const { stringify } = require('querystring');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Email address is required',
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+
+    }
+)
